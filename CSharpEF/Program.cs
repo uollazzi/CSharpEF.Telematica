66b21a21f8f19db35b48f1b0cc54c427ad2283b0
Matteo Valenzi <dilloawallace@hotmail.com>
2025-11-27 09:42:05 +0100

    .

--- CSharpEF/Program.cs
+++ CSharpEF/Program.cs
@@ -15,5 +15,30 @@
 // Da eseguire nel Package Manager Console:
 // Scaffold-DbContext -DataAnnotations -OutputDir Models -ContextDir . 'Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CSharpEF2;Integrated Security=true;' Microsoft.EntityFrameworkCore.SqlServer
 
-var connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=CSharpEF2;Integrated Security=true;";
+// prima eseguire sul terminale: dotnet tool install --global dotnet-ef
+// Da eseguire nel terminale della cartella del progetto:
+// dotnet ef dbcontext scaffold "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CSharpEF2;Integrated Security=true;" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --output-dir Models --context-dir .
+
+
+using CSharpEF;
+
+using (var dc = new CsharpEfContext())
+{
+    Console.WriteLine("Elenco Todos");
+    foreach (var todo in dc.Todos)
+    {
+        Console.WriteLine(todo.Testo);
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Elenco Categorie e Todos");
+    foreach (var cat in dc.Categories)
+    {
+        Console.WriteLine(cat.Nome);
+        foreach (var todo in cat.Todos)
+        {
+            Console.WriteLine("\t" + todo.Testo);
+        }
+    }
+}
 
